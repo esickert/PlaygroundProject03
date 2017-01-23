fff60fbb09ac30de5876cf62e7693a8206601547
esickert <esickert@gmail.com>
2017-01-23 14:23:59 -0800

    plus

--- src/csvReader/CSVReader.java
+++ src/csvReader/CSVReader.java
@@ -26,15 +26,15 @@ public class CSVReader	{
 	        Scanner sc = new Scanner(file);
 
 //	       while (sc.hasNextLine()) {
-	       for(int x = 0; x <= 5; x++)	{              //this allows me to see the output at each set of the loop
-	           String str = sc.nextLine(); //this stores one line of the input file.
+	       for(int x = 0; x <= 5; x++)	{                                    //this allows me to see the output at each set of the loop
+	           String str = sc.nextLine();                                   //this stores one line of the input file.
 	          
-	           String[] resultArray = str.split(",");  //SO THIS STORES THE SPLIT INPUT LINE!!!!!!!!!!1
+	           String[] resultArray = str.split(",");                        //SO THIS STORES THE SPLIT INPUT LINE!!!!!!!!!!1
 	           for(int i = 0; i <= resultArray.length - 1; i++)    {
 	               System.out.print(resultArray[i] + " ");  
 	            }
 	            System.out.println();
-	            result = resultArray;  //i think there is alot of redundancy here. this is in the while loop. THIS IS FUCKED UP!!!!!!!!!!!!!!!!!!!!!!
+	            result = resultArray;                                        //i think there is alot of redundancy here. this is in the while loop. THIS IS FUCKED UP!!!!!!!!!!!!!!!!!!!!!!
 	            
 	            
 	        }  //end of while  **********************************************
