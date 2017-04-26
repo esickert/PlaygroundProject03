4adc04b4526755449bca42b459fd28f653ea90a4
esickert <esickert@gmail.com>
2017-04-26 15:16:46 -0700

    plus

--- src/csvReader/CSVReader.java
+++ src/csvReader/CSVReader.java
@@ -48,6 +48,7 @@ public class CSVReader {
 	          
 	           String[] resultArray = str.split(",");                        //SO THIS STORES THE SPLIT INPUT LINE
 	           for(int i = 0; i <= resultArray.length - 1; i++)    {
+//	        	   for(int j; j <= anArray[][]
 	               System.out.print(resultArray[i] + "\t");  
 	            }
 	            System.out.println();
